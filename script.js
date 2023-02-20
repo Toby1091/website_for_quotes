09e51a4c6e0c4cd031bc94a177343a2194959e9e
Toby1091 <112862755+Toby1091@users.noreply.github.com>
2023-02-20 21:57:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const QUOTES_CSV_URL = 'quotes.csv';
 async function fetchQuotes() {
   const response = await fetch(QUOTES_CSV_URL);
   const text = await response.text();
-  const quotes = text.split('\n').map(quote => quote.slice(1, -1));
+  const quotes = text.split('\n').map(quote => quote.slice(2, -10));
   return quotes;
 }
 
