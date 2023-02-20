19f0682ff697988721a9e25c4e13fedccadbba9e
Toby1091 <112862755+Toby1091@users.noreply.github.com>
2023-02-20 20:56:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,28 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>Quote of the Day</title>
-  <style>
-    body {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-      margin: 0;
-      padding: 0;
-      font-family: Arial, sans-serif;
-      text-align: center;
-    }
-    #quote {
-      max-width: 800px;
-      margin: auto;
-    }
-    #quote p {
-      font-size: 2em;
-      line-height: 1.2em;
-      margin: 0;
-      padding: 20px;
-    }
-  </style>
-</head>
-<body>
-  <div id="quote">
-    <p id="quote-text"></p>
-    <p id="quote-author"></p>
-  </div>
+// Replace the URL below with the path to your quotes.csv file.
+const QUOTES_CSV_URL = 'quotes.csv';
 
-  <script src="index.js"></script>
-</body>
-</html>
+// Retrieve the CSV file as text and parse it into an array of quote objects.
+async function fetchQuotes() {
+  const response = await fetch(QUOTES_CSV_URL);
+  const text = await response.text();
+  const rows = text.trim().split('\n');
+  const header = rows.shift();
+  const quotes = rows.map(row => {
+    const cells = row.split(',');
+    return { text: cells[0].replace(/"/g, ''), author: cells[1].replace(/"/g, '') };
+  });
+  return quotes;
+}
+
+// Display a random quote from the list of quotes.
+async function displayQuote() {
+  const quotes = await fetchQuotes();
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
+  const quoteText = document.getElementById('quote-text');
+  const quoteAuthor = document.getElementById('quote-author');
+  quoteText.textContent = quote.text;
+  quoteAuthor.textContent = `- ${quote.author}`;
+}
+
+// Display a quote when the page is first loaded.
+displayQuote();
