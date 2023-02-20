53d7813b3ac5a476a3d85a12589d2d9263200b36
Toby1091 <112862755+Toby1091@users.noreply.github.com>
2023-02-20 21:39:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-// Replace the URL below with the path to your quotes.csv file.
-const QUOTES_CSV_URL = 'quotes.csv';
+// Replace the URL below with the path to your quotes.txt file.
+const QUOTES_URL = 'quotes.txt';
 
-// Retrieve the CSV file as text and parse it into an array of quote objects.
+// Retrieve the quotes as text and parse them into an array of strings.
 async function fetchQuotes() {
-  const response = await fetch(QUOTES_CSV_URL);
+  const response = await fetch(QUOTES_URL);
   const text = await response.text();
-  const quotes = text.trim().split('\n').map(row => {
-    return { text: row.trim() };
-  });
+  const quotes = text.trim().split('\n');
   return quotes;
 }
-
-
 
 // Display a random quote from the list of quotes.
 async function displayQuote() {
   const quotes = await fetchQuotes();
   const quote = quotes[Math.floor(Math.random() * quotes.length)];
   const quoteText = document.getElementById('quote-text');
-  const quoteAuthor = document.getElementById('quote-author');
-  quoteText.textContent = quote.text;
-  quoteAuthor.textContent = quote.author;
+  quoteText.textContent = quote;
 }
 
 // Display a quote when the page is first loaded.
