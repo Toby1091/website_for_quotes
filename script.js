dcbe84f78846c75e463ef8da40afb261e67993a7
Toby1091 <112862755+Toby1091@users.noreply.github.com>
2023-02-20 20:44:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-// Replace the URL below with the path to your quotes.csv file.
-const QUOTES_CSV_URL = 'quotes.csv';
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Quote of the Day</title>
+  <style>
+    body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+      padding: 0;
+      font-family: Arial, sans-serif;
+      text-align: center;
+    }
+    #quote {
+      max-width: 800px;
+      margin: auto;
+    }
+    #quote p {
+      font-size: 2em;
+      line-height: 1.2em;
+      margin: 0;
+      padding: 20px;
+    }
+  </style>
+</head>
+<body>
+  <div id="quote">
+    <p id="quote-text"></p>
+    <p id="quote-author"></p>
+  </div>
 
-// Retrieve the CSV file as text and parse it into an array of quote objects.
-async function fetchQuotes() {
-  const response = await fetch(QUOTES_CSV_URL);
-  const text = await response.text();
-  const rows = text.trim().split('\n');
-  const header = rows.shift();
-  const quotes = rows.map(row => {
-    const cells = row.split(',');
-    return { text: cells[0], author: cells[1] };
-  });
-  return quotes;
-}
-
-// Display a random quote from the list of quotes.
-async function displayQuote() {
-  const quotes = await fetchQuotes();
-  const quote = quotes[Math.floor(Math.random() * quotes.length)];
-  const quoteText = document.getElementById('quote-text');
-  const quoteAuthor = document.getElementById('quote-author');
-  quoteText.textContent = quote.text;
-  quoteAuthor.textContent = quote.author;
-}
-
-// Display a new quote when the "New Quote" button is clicked.
-const newQuoteButton = document.getElementById('new-quote-button');
-newQuoteButton.addEventListener('click', displayQuote);
-
-// Display a quote when the page is first loaded.
-displayQuote();
+  <script src="index.js"></script>
+</body>
+</html>
